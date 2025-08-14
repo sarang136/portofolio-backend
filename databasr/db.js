596d8f9a191e95623a6dde662b9be6d14aeb01c2
gopi-gh <gopi-gh@d3vtech.com>
2025-08-14 18:01:46 +0530

    New updates

--- databasr/db.js
+++ databasr/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDb = async () => {
-   await mongoose.connect('mongodb+srv://sarang:x9ye8VZyMhMayYn7@cluster0.xa4bkck.mongodb.net/Portfolio')
+   await mongoose.connect('mongodb+srv://harshadbankar1596:harshadbankar@cluster0.fx9sgmn.mongodb.net/Portfolio')
 }
 
 module.exports = connectDb; 
